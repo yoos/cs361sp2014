b596d533f21dc1626083b78f4c854196c341b96d
Soo-Hyun Yoo <yoos117@gmail.com>
2014-05-28 20:38:39 -0700

    Merge main.

--- main.php
+++ main.php
@@ -2,10 +2,8 @@
 require_once('io.php');
 ?>
 
-<p>
-	
-<h3>Record Audio</h3>	
-	
+<h3>Record Audio</h3>
+
 <form action="" method="post">
 
 	Transcript:<br/>
@@ -16,28 +14,4 @@ require_once('io.php');
 	<input type="submit" value="Record" id="record-submit">
 
 </form>
-
-	
-Sentence log test:
-
-<?php
-$audio_id = log_sentence(0, "test");
-?>
-</p>
-
-<p>
-Word tag test:
-
-<?php
-tag_word($audio_id, "car", 1.2, 3.4);
-?>
-</p>
-
-<p>
-Word retrieval test:
-
-<?php
-get_word("banana");
-?>
-</p>
 
