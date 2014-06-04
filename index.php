f86a53e13dab74e2e439997d179cb99618e041c0
dropacid <rixempire@gmail.com>
2014-06-04 12:05:13 -0400

    error

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 
 		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
 		<link rel="stylesheet" type="text/css" href="bootstrap.min.css" />
-		<script type="text/javascript" src="/jquery-2.1.1.min.js" ></script>	
-		<script type="text/javascript" src="/application.js" ></script>	
+		<script type="text/javascript" src="jquery-2.1.1.min.js" ></script>	
+		<script type="text/javascript" src="application.js" ></script>	
 		<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>	
 
 
